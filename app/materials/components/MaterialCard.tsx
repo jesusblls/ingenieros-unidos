761c568e51cad688d051e8a94afa5e03b0bad6a1
jesus <jesus.castillohdz98@gmail.com>
2023-10-14 13:39:46 -0600

    add modal to see detailed material

--- app/materials/components/MaterialCard.tsx
+++ app/materials/components/MaterialCard.tsx
@@ -8,6 +8,7 @@ import { CartContext } from "@/app/context/CartContext";
 import { IoRemove } from "react-icons/io5";
 import { BsTrash } from "react-icons/bs";
 import axios from "axios";
+import MaterialDetailModal from "./MaterialDetailModal";
 
 interface MaterialWithUser extends Material {
   user: User;
@@ -23,6 +24,7 @@ const MaterialCard: React.FC<MaterialCardProps> = ({
 }) => {
   const router = useRouter();
   const { addToCart } = useContext(CartContext);
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleAddToCart = (item: Material) => {
     addToCart(item);
@@ -40,15 +42,22 @@ const MaterialCard: React.FC<MaterialCardProps> = ({
       });
   };
 
+  const handleOpenModal = () => {
+    setIsOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsOpen(false);
+  };
+
   return (
     <div className="w-full max-w-md bg-white border border-gray-200 rounded-lg shadow">
-      <a href="#">
-        <img
-          className="rounded-lg w-full h-64 object-cover"
-          src={material.image || "https://via.placeholder.com/300"}
-          alt="material image"
-        />
-      </a>
+      <img
+        className="rounded-lg w-full h-64 object-cover"
+        src={material.image || "https://via.placeholder.com/300"}
+        alt="material image"
+        onClick={handleOpenModal}
+      />
       <div className="px-5 pb-2 mt-3">
         <a href="#">
           <h5 className="text-xl font-bold tracking-tight text-gray-90">
@@ -74,6 +83,11 @@ const MaterialCard: React.FC<MaterialCardProps> = ({
           )}
         </div>
       </div>
+      <MaterialDetailModal
+        material={material}
+        isOpen={isOpen}
+        onClose={handleCloseModal}
+      />
     </div>
   );
 };
---
+++ app/materials/components/MaterialDetailModal.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import Modal from "@/app/components/modals/Modal";
+import React, { useContext, useState } from "react";
+import Image from "next/image";
+import { CartContext } from "@/app/context/CartContext";
+import { FiMessageCircle } from "react-icons/fi";
+import { BsMessenger, BsTrash } from "react-icons/bs";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import Button from "@/app/components/Button";
+
+interface MaterialDetailModalProps {
+  isOpen?: boolean;
+  onClose: () => void;
+  material: any;
+}
+
+const MaterialDetailModal: React.FC<MaterialDetailModalProps> = ({
+  isOpen,
+  onClose,
+  material,
+}) => {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <div className="space-y-12">
+        <div className="border-gray-900/10 pb-12">
+          <h2
+            className="
+                            text-base 
+                            font-semibold 
+                            leading-7 
+                            text-gray-900
+                        "
+          >
+            {material.name}
+          </h2>
+          <p className="mt-1 text-sm leading-6 text-gray-600">
+            Descrioción: {material.description}
+          </p>
+
+          <div className="mt-10 flex flex-col gap-y-8">
+            <div
+              className="flex items-center justify-between"
+              key={material.id}
+            >
+              <div className="flex items-center">
+                <Image
+                  src={material.image || "https://via.placeholder.com/300"}
+                  width={150}
+                  height={150}
+                  className="rounded-lg object-cover"
+                  alt="material image"
+                />
+                <div className="ml-4">
+                  <p className="text-sm font-semibold text-gray-900">
+                    Vendedor: {material.user.name}
+                  </p>
+                  <p className="text-sm text-gray-500">
+                    Correro: {material.user.email}
+                  </p>
+                </div>
+              </div>
+              {/* <div className="flex items-center gap-x-2">
+                <Button>
+                  <BsMessenger className="w-5 h-5" />
+                  Contactar
+                </Button>
+                <Button danger>
+                  <BsTrash className="w-5 h-5" />
+                  Eliminar
+                </Button>
+              </div> */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+export default MaterialDetailModal;
